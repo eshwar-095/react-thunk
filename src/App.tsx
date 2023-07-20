36191e864beca665a621939a169bfa07a6cd4d27
eshwar-095 <eshwar095@gmail.com>
2023-07-20 18:58:11 +0530

    added required packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
+import store from './store';
+import Login from './pages/Login';
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
-      <header className="App-header">
+      <div id="route-container">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Login></Login>
+      </div>
     </div>
+  </Provider>
   );
 }
 
